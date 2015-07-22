9e657a832a84edb10278eb61b47381670f4dc1bc
Pavel Trutman <pavel.trutman@fel.cvut.cz>
2015-07-22 15:06:12 +0200

    Asterixed local norm coded.

--- utils.py
+++ utils.py
@@ -5,4 +5,13 @@ from numpy import *
 class Utils:
 
   def LocalNorm(u, hessian):
+    """
+    Nesterov, p.181
+    """
     return float(sqrt((hessian*u).T*u))
+
+  def LocalNormA(u, hessian):
+    """
+    Nesterov, p.181
+    """
+    return float(sqrt((hessian.I*u).T*u))
