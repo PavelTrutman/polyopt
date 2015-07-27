d8a2974c759a9ff8ce6754556504a64d827bd3b2
Pavel Trutman <pavel.trutman@fel.cvut.cz>
2015-07-27 10:55:37 +0200

    Main-following scheme in MATLAB.

--- matlab.m
+++ matlab.m
@@ -1,3 +1,4 @@
+c = [3; -2];
 A0 = [1 1 0;
       1 1 0;
       0 0 0];
@@ -55,3 +56,47 @@ while true
   
 end
 
+% prepare x
+x = y - H\g
+
+% initialization of the iteration process
+t = 0;
+eps = 10^(-3);
+k = 0;
+
+F = eye(3) + A0*x(1) + A1*x(2);
+Fi = inv(F);
+Fi0 = A0*Fi;
+Fi1 = A1*Fi;
+g0 = -trace(Fi0);
+g1 = -trace(Fi1);
+g = [g0; g1];
+h00 = trace(Fi0^2);
+h11 = trace(Fi1^2);
+h01 = trace(Fi0*Fi1);
+H = [h00 h01; h01, h11];
+
+sqrt((H\g)'*g)
+
+while true
+  k = k + 1
+  F = eye(3) + A0*x(1) + A1*x(2);
+  Fi = inv(F);
+  Fi0 = A0*Fi;
+  Fi1 = A1*Fi;
+  g0 = -trace(Fi0);
+  g1 = -trace(Fi1);
+  g = [g0; g1]
+  h00 = trace(Fi0^2);
+  h11 = trace(Fi1^2);
+  h01 = trace(Fi0*Fi1);
+  H = [h00 h01; h01 h11]
+
+  t = t + gamma/sqrt((H\c)'*c)
+  x = x - H\(t*c+g)
+  
+  if eps*t >= nu + (beta + sqrt(nu))*beta/(1 - beta)
+    break;
+  end
+  
+end
