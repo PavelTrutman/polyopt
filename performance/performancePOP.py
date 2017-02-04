45314d512b6e62fb7893c93387a48561e5c3b69a
Pavel Trutman <pavel.trutman@fel.cvut.cz>
2017-02-04 16:47:48 +0100

    POP performance measuring improved and reworked.

--- performance/performancePOP.py
+++ performance/performancePOP.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 
 """
-Preformance measuring and saves results into file.
+Preformance measuring for POP solvers, saves results into file and plots them.
 
 by Pavel Trutman, pavel.trutman@fel.cvut.cz
 """
 
 from numpy import *
 from numpy.random import uniform
-from POPSolver import POPSolver
+from scipy.misc import comb
+from polyopt import POPSolver
 import timeit
-
+import gnuplot as gp
+import tempfile
+import argparse
 
 def generateVariablesDegree(d, n):
   """
@@ -67,71 +70,171 @@ def generateVariablesUpDegree(d, n):
   return variables
 
 
+# command line argument parser
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument('--help', '-h', action='help', help='show this help message and exit')
+parser.add_argument('--bench', '-b', action='store_true', help='run benchmark')
+parser.add_argument('--plot', '-p', action='store_true', help='plot results')
+plotGroup = parser.add_argument_group('plot options', 'List of available graphs when --plot argument active')
+plotGroup.add_argument('--relax-order', action='store_true', help='graph based on the relaxation order', dest='relaxOrder')
+plotGroup.add_argument('--poly-degree', action='store_true', help='graph based on the degree of polynomials', dest='polynomialDegree')
+plotGroup.add_argument('--num-variables', action='store_true', help='graph based on the number of variables', dest='dimension')
+args = parser.parse_args()
 
-# dimension: 1, 2, 3
-dim = range(1, 4)
 
-# degree of generated polynomials: 2, 3, 4
-degF = range(2, 5)
-
-# relaxation orders: ceil(degF/2), ceil(degF/2) + 1, ceil(degF/2) + 2, ceil(degF/2) + 3
-order = range(0, 4)
-
-# number of feasible point generations: 10
-N = range(0, 10)
-
-# initialize the array with result times
-results = empty((max(dim) + 1, max(degF) + 1, len(order)))
-results.fill(nan)
-
-# for dimension
-for n in dim:
-
-  #for degree of polynomials
-  for dF in degF:
-    variables = generateVariablesUpDegree(dF, n)
-
-    # generate objective function
-    f = {}
-    for v in variables:
-      f[v] = uniform(-1, 1)
-
-    #generate constraining function
-    t = [0]*n
-    g = {tuple(t): 1}
-    for i in range(0, n):
+# benchmarking
+if args.bench:
+  # dimension: 1, 2, 3
+  dim = range(1, 4)
+  
+  # degree of generated polynomials: 2, 3, 4
+  degF = [2, 4, 6]
+  
+  # relaxation orders: ceil(degF/2), ceil(degF/2) + 1, ceil(degF/2) + 2, ceil(degF/2) + 3
+  order = range(0, 2)
+  
+  # number of feasible point generations: 10
+  N = range(0, 20)
+  
+  # initialize the array with result times
+  results = empty((max(dim) + 1, max(degF) + 1, len(order)))
+  results.fill(nan)
+  
+  # for dimension
+  for n in dim:
+  
+    #for degree of polynomials
+    for dF in degF:
+      variables = generateVariablesUpDegree(dF, n)
+  
+      # generate objective function
+      f = {}
+      for v in variables:
+        f[v] = uniform(-1, 1)
+  
+      #generate constraining function
       t = [0]*n
-      t[i] = 2
-      g[tuple(t)] = -1
+      g = {tuple(t): 1}
+      for i in range(0, n):
+        t = [0]*n
+        t[i] = 2
+        g[tuple(t)] = -1
+  
+      minD = int(ceil(dF/2))
+  
+      # for relaxation order
+      for dR in order:
+        d = dR + minD
+        print((n, dF, dR), ', SDP dim: {:3d} '.format(int(comb(n+2*d, n))), sep='', end='', flush=True)
+  
+        # clear measured time
+        t = []
+  
+        # for different feasible points
+        for i in N:
+          # initialize the solver
+          problem = POPSolver(f, g, d)
+  
+          # obtain some feasible point for the SDP problem
+          y0 = problem.getFeasiblePoint(1)
+  
+          # startup time
+          timeStart = timeit.default_timer()
+  
+          #solve the problem
+          problem.solve(y0)
+  
+          # final time
+          t.append(timeit.default_timer() - timeStart)
 
-    minD = int(ceil(dF/2))
+          print('.', end='', flush=True)
+  
+        print(' {:3.2f} s'.format(nanmedian(t)), sep='')
+        results[n, dF, dR] = nanmedian(t)
+  
+  save('data/POP.npy', results)
 
-    # for relaxation order
-    for dR in order:
-      d = dR + minD
 
-      # clear measured time
-      t = 0
-
-      # for different feasible points
-      for i in N:
-        # startup time
-        timeStart = timeit.default_timer()
-
-        # initialize the solver
-        problem = POPSolver(f, g, d)
-
-        # obtain some feasible point for the SDP problem
-        y0 = problem.getFeasiblePoint(1)
-
-        #solve the problem
-        problem.solve(y0)
-
-        # final time
-        t += timeit.default_timer() - timeStart
-
-      print((n, dF, dR))
-      results[n, dF, dR] = t/len(N)
-
-save('performanceResults/performanceResults.npy', results)
-print(results)
+# plotting
+if args.plot:
+  # load data form file
+  data = load('data/POP.npy')
+  print(data)
+  shape = data.shape
+  
+  # based on dimension
+  if args.dimension:
+    # start GnuPlot
+    gnuplot = gp.Gnuplot()
+    gnuplot('set xlabel "Dimension"')
+    gnuplot('set ylabel "Time [s]"')
+    gnuplot('set title "Performance of the POP solver based on the number of variables."')
+    
+    # plot data
+    tempFiles = []
+    for df in range(0, shape[1]):
+      for dr in range(0, shape[2]):
+        dataSliced = data[:, df, dr]
+        noNan = nonzero([~isnan(dataSliced)])[1]
+        a = dataSliced[noNan]
+        if a.shape[0] != 0:
+          plotFile = tempfile.NamedTemporaryFile()
+          tempFiles.append(plotFile)
+          plot = gnuplot.replot(gp.Data(noNan, a, title = 'df = ' + str(df) + ', relaxOrder = ' + str(dr), with_ = 'lines lw 1.5', filename=plotFile.name))
+    print('\nPress enter to continue')
+    input()
+  
+    # close temp file
+    for tempFile in tempFiles:
+      tempFile.close()
+          
+  # based on relaxation order
+  if args.relaxOrder:
+    # start GnuPlot
+    gnuplot = gp.Gnuplot()
+    gnuplot('set xlabel "Relaxation order"')
+    gnuplot('set ylabel "Time [s]"')
+    gnuplot('set title "Performance of the POP solver based on the relaxation order."')
+  
+    # plot data
+    tempFiles = []
+    for n in range(0, shape[0]):
+      for df in range(0, shape[1]):
+        if isnan(data[n, df, :]).any():
+          continue
+        plotFile = tempfile.NamedTemporaryFile()
+        tempFiles.append(plotFile)
+        plot = gnuplot.replot(gp.Data(range(0, data.shape[2]), data[n, df, :], title = 'dim = ' + str(n) + ', df = ' + str(df), with_ = 'lines lw 1.5', filename=plotFile.name))
+    print('\nPress enter to continue')
+    input()
+  
+    # close temp file
+    for tempFile in tempFiles:
+      tempFile.close()
+  
+  # based on polynomial degree
+  if args.polynomialDegree:
+    # start GnuPlot
+    gnuplot = gp.Gnuplot()
+    gnuplot('set xlabel "Degree of the polynomial"')
+    gnuplot('set ylabel "Time [s]"')
+    gnuplot('set title "Performance of the POP solver based on the degree of polynomials."')
+  
+    # plot data
+    tempFiles = []
+    for n in range(0, shape[0]):
+      dataSliced = data[n, :, :]
+      noNan = nonzero([~isnan(dataSliced).any(axis=1)])[1]
+      a = dataSliced[noNan, :]
+      if a.shape[0] != 0:
+        for relaxOrder in range(0, shape[2]):
+          
+          plotFile = tempfile.NamedTemporaryFile()
+          tempFiles.append(plotFile)
+          plot = gnuplot.replot(gp.Data(noNan, a[:, relaxOrder], title = 'dim = ' + str(n) + ', relaxOrder = ' + str(relaxOrder), with_ = 'lines lw 1.5', filename=plotFile.name))
+    print('\nPress enter to continue')
+    input()
+  
+    # close temp file
+    for tempFile in tempFiles:
+      tempFile.close()
