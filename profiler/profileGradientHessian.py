d56358c3714d226c22a1cc105bf1d940a0c4d471
Pavel Trutman <pavel.trutman@fel.cvut.cz>
2017-02-07 17:30:33 +0100

    Added profilers for the gradientHessian function.

---
+++ profiler/profileGradientHessian.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import timeit
+
+repeat = 35;
+count = 35;
+
+setup = '''
+from polyopt.utils import Utils
+import pickle
+with open('AAll.pickle', 'rb') as f:
+  AAll = pickle.load(f)
+with open('x.pickle', 'rb') as f:
+  x = pickle.load(f)
+'''
+
+t = timeit.Timer('Utils.gradientHessian(AAll, x)', setup=setup)
+times = t.repeat(repeat, count)
+print(times)
+print('{:4.3g} ms'.format(min(times)/count*1000))
--- profiler/profileSDP.py
+++ profiler/profileSDP.py
@@ -4,42 +4,60 @@ import pycallgraph
 from numpy import *
 from numpy.linalg import *
 from polyopt import SDPSolver
-from polyopt.utils import Utils
+import pickle
+import re
+
+def parseDot(regexp, content):
+  m = regexp.search(content)
+  return float(m.group(2))/float(m.group(1).replace(' ', '')) # removing &nbsp;
 
 
-def filtercalls(call_stack, modul, clas, func, full):
-    mod_ignore = ['shutil','scipy.optimize','re','os','sys','json']
-    func_ignore = ['CustomFunctionName','pdbcall']
-    clas_ignore = ['pdb']
-    return modul not in mod_ignore and func not in func_ignore and clas not in clas_ignore
+# number of iterations
+N = 5
 
+# function for which get the timings
+functionsToMeasure = ['polyopt.utils.gradientHessian', 'numpy.core.fromnumeric.trace']
+regexps = list(map((lambda x: re.compile(r".*" + x.replace('.', r"\.") + r".*calls:\s*([0-9 ]*).n.*time:\s*([0-9.]*)s")), functionsToMeasure))
 
 # specify dimensions
-dim = 37
+dim = 50
 
 # starting point
-startPoint = zeros((dim, 1));
+startPoint = zeros((dim, 1))
 
 # objective function
 c = ones((dim, 1))
 
 # get LMI matrices
-A = [identity(dim)];
-for i in range(0, dim):
-  A.append(Utils.randomSymetric(dim))
+with open('matrices.pickle', 'rb') as f:
+  A = pickle.load(f)
 
-# init SDP program
-problem = SDPSolver(c, [A])
+times = empty((len(functionsToMeasure), N))
+for i in range(0, N):
+  # init SDP program
+  problem = SDPSolver(c, [A])
+  
+  # bound the problem
+  problem.bound(1)
+  
+  # pycallgraph config
+  config = pycallgraph.Config()
+  config.include_stdlib = True
+  dot = pycallgraph.output.GraphvizOutput(output_file='profile.dot', output_type='dot')
+  graphviz = pycallgraph.output.GraphvizOutput(output_file='profile.png')
+  
+  # profile
+  with pycallgraph.PyCallGraph(output=[graphviz, dot], config=config) as graph:
+    # solve
+    problem.solve(startPoint, problem.dampedNewton)
 
-# bound the problem
-problem.bound(1)
+  with open('profile.dot', 'r') as f:
+    content = f.read()
+  
+  times[:, i] = array(list(map((lambda r: parseDot(r, content)), regexps)))
+  print('.', end='', flush=True)
 
-# pycallgraph config
-config = pycallgraph.Config()
-config.include_stdlib = True
-graphviz = pycallgraph.output.GraphvizOutput(output_file='profile.png')
-
-# profile
-with pycallgraph.PyCallGraph(output=graphviz, config=config):
-  # solve
-  problem.solve(startPoint, problem.dampedNewton)
+print()
+times = nanmedian(times, axis=1)
+for i in range(0, len(functionsToMeasure)):
+  print('{:>30s}: {:4.3g} ms'.format(functionsToMeasure[i], times[i]*1000))
